bee46a4de5d8bbaf971b033e834b3f472bec5e94
WouRaoyu <petitwhy@gmail.com>
2020-06-21 11:45:12 +0800

    Add gltf support to the project

--- build.bat
+++ build.bat
@@ -10,11 +10,13 @@ cmake -G "Visual Studio 15 2017" -A x64 ^
  -DLIBXML2_LIBRARIES="%CONDA_PREFIX%\Library\lib\libxml2.lib" ^
  -DOCC_INCLUDE_DIR="%CONDA_PREFIX%\Library\include\opencascade" ^
  -DOCC_LIBRARY_DIR="%CONDA_PREFIX%\Library\lib" ^
+ -DJSON_INCLUDE_DIR="%CONDA_PREFIX%\Library\include" ^
  -DCOLLADA_SUPPORT=Off ^
  -DBUILD_IFCPYTHON=off ^
  -DBUILD_EXAMPLES=On ^
  -DBUILD_GEOMSERVER=Off ^
  -DBUILD_CONVERT=On ^
+ -DGLTF_SUPPORT=On ^
  ../cmake
 
 REM cmake --build . --target INSTALL --config RelWithDebInfo
